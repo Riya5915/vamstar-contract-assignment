c59b5e08bb066d546daf6be238f348d87399e090
Riya Sharma <sriya5915@gmail.com>
2021-01-23 22:18:46 +0530

    complete assignment

--- index.js
+++ index.js
@@ -15,3 +15,22 @@ console.log("Object: "+(document.querySelector("#block-views-block-view-noticia-
 console.log("Links: "+(document.querySelector("#block-views-block-view-noticia-pbh-block-5 table div h2 a").href));
 // output: Links: https://prefeitura.pbh.gov.br/taxonomy/term/4597 
 
+// alternative approach
+// function links() {
+//     var input = []; // initialise an empty array
+//     var temp = '';
+//     do {
+//         temp = document.querySelector("#block-views-block-view-noticia-pbh-block-5 table a").href;
+//         if (temp === "" || temp === null) {
+//             break;
+//         } else {
+//             input.push(temp);  // the array will dynamically grow
+//         }
+//         console.log("done");
+//     } while (temp !== undefined);
+
+//     for(let i = 0; i< input.length; i++){
+//         console.log(input[i]);
+//     }
+
+// }
